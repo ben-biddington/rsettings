5684f5055d5cacca1a3ca652698f3351f39a008f
Ben Biddington <ben.biddington@gmail.com>
2013-11-27 12:35:42 +1300

    It fails if setting is missing

--- lib/settings/adapters/environment_settings.rb
+++ lib/settings/adapters/environment_settings.rb
@@ -1,5 +1,11 @@
 class EnvironmentSettings
   def method_missing(name, *args)
-    ENV[name.to_s]
+    ENV[name.to_s].tap{|it| ___fail_because_setting_is_missing(name) if it.nil?}
+  end
+
+  private
+
+  def ___fail_because_setting_is_missing(name)
+    fail "Setting <#{name}> not found"
   end
 end
--- spec/system.tests/can_use_env_spec.rb
+++ spec/system.tests/can_use_env_spec.rb
@@ -16,6 +16,12 @@ describe "Can use environment variables as a settings list" do
     expect(Settings.new.name).to eql "bangerman"
   end
 
-  it "does what when setting does not exist?"
+  it "fails when setting does not exist" do
+    ENV.clear
+    expect{Settings.new.name}.to raise_error /Setting <name> not found/
+  end
+
   it "does type matter? Ought I be able to expect numbers for example. Currently all are strings."
+
+  # Can't use metaclass because class < Object which has lots of methods defined. BasicObject is what we want.
 end
