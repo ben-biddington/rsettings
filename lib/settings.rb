7448fc566632cc39e3bb4d7478f2e65ca64078c6
Ben Biddington <ben.biddington@gmail.com>
2013-11-27 11:47:32 +1300

    Settings just delegates

--- lib/settings.rb
+++ lib/settings.rb
@@ -1,9 +1,10 @@
 dir = File.join(File.dirname(__FILE__))
 
-Dir.glob(File.join(dir, "settings", "*.rb")).each {|f| include f}
+Dir.glob(File.join(dir, "settings", "**", "*.rb")).each {|f| require f}
 
 class Settings
   class << self
-    def name; ENV["name"]; end
+    require "forwardable"; extend Forwardable
+    delegate :name => EnvironmentSettings
   end
 end
---
+++ lib/settings/adapters/environment_settings.rb
@@ -0,0 +1,5 @@
+class EnvironmentSettings
+  class << self
+    def name; ENV["name"]; end
+  end
+end
