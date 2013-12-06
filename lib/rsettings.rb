6de157c57ca3621ba9353daa05055b54bcc0498e
Ben Biddington <ben.biddington@gmail.com>
2013-12-06 20:37:03 +1300

    [GREEN] can supply mappings

--- lib/rsettings.rb
+++ lib/rsettings.rb
@@ -3,6 +3,10 @@ dir = File.join(File.dirname(__FILE__))
 Dir.glob(File.join(dir, "rsettings", "**", "*.rb")).each {|f| require f}
 
 class Settings
+  def initialize
+    @config = SettingsConfiguration.new
+  end
+
   def configure(&block)
     @config = SettingsConfiguration.new.tap do |conf|
       conf.instance_exec &block if block_given?
@@ -11,13 +15,23 @@ class Settings
 
   def method_missing(m, *args)
     fail "Only support queries, cannot do <#{m}>" unless args.empty?
-    EnvironmentSettings.new.get m
+    
+    setting_name = @config.name_for m
+    
+    EnvironmentSettings.new.get setting_name
   end
 end
 
 class SettingsConfiguration
-  def let(opts ={})
-    puts "opts => #{opts}"
-    (@opts ||= {}).merge opts
+  def let(opts = {})
+    _opts.merge! opts.invert
   end
+
+  def name_for(setting)
+    _opts[setting] || setting
+  end
+
+  private
+
+  def _opts; @opts ||= {}; end
 end
--- spec/system.tests/can_use_env_spec.rb
+++ spec/system.tests/can_use_env_spec.rb
@@ -18,7 +18,7 @@ describe "Can use environment variables as a settings list" do
 
   it "does type matter? Ought I be able to expect numbers for example. Currently all are strings."
   
-  it "can supply mappings somehow" do
+  it "can supply mappings" do
     settings = Settings.new
 
     settings.configure do
@@ -33,7 +33,9 @@ describe "Can use environment variables as a settings list" do
     expect(settings.password).to eql "allblacks"
   end
 
+  it "when you have mappings it still finds unmapped ones"
   it "make sure you can use setting name 'configure'"
+  it "what about a setting caled initialize?"
   it "fails if you try and configure without a block"
 
   # Can't use metaclass because class < Object which has lots of methods defined. BasicObject is what we want.
