d28885270b7926a90d5bdb02e6f2c9d0ba794226
Ben Biddington <ben.biddington@gmail.com>
2013-12-11 12:01:21 +1300

    you get the environment setting when it exists

--- spec/system.tests/can_fall_back_to_file_settings_spec.rb
+++ spec/system.tests/can_fall_back_to_file_settings_spec.rb
@@ -1,20 +1,28 @@
 require "spec_helper"
 
 describe "a settings fallback" do
-  it "for example, you can fall back to a setting on disk" do
-    settings = Settings.new do
-      with_settings :chain => [EnvironmentSettings, BasicDiskSettings]
-    end
-
+  before do
     ENV.clear
 
     BasicDiskSettings.new do
       clear
       set :jazzs_bike_shorts => "tight"
     end
+  end
 
+  let(:settings) do 
+    Settings.new do
+      with_settings :chain => [EnvironmentSettings, BasicDiskSettings]
+    end
+  end
+
+  it "for example, you can fall back to a setting on disk" do
     expect(settings.jazzs_bike_shorts).to eql "tight"
   end
 
-  it "by default you get environment settings only"
+  it "you get the environment setting when it exists" do
+    ENV["jazzs_bike_shorts"] = "this is from the environment variables"
+
+    expect(settings.jazzs_bike_shorts).to eql "this is from the environment variables"
+  end
 end
