c9a12f00495ee5c81162992c48ee7ee2889bdf4e
Ben Biddington <ben.biddington@gmail.com>
2013-12-11 16:20:20 +1300

    Formatting

--- lib/rsettings.rb
+++ lib/rsettings.rb
@@ -10,6 +10,10 @@ class Settings
   def method_missing(m, *args, &block)
     fail "Only support queries, cannot do command <#{m}>" unless args.empty?
 
-    RSettings.new(:settings => @conf.settings,:missing => @conf.missing,:names => @conf.names).find m
+    RSettings.new(
+      :settings => @conf.settings,
+      :missing => @conf.missing,
+      :names => @conf.names
+    ).find m
   end
 end
