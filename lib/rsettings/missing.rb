33baf12fb7d4a9cb01ba18e0dc1175f44f113acb
Ben Biddington <ben.biddington@gmail.com>
2013-12-12 19:14:16 +1300

    How to handle missing settings is also a public idea

---
+++ lib/rsettings/missing.rb
@@ -0,0 +1,25 @@
+module Missing
+  def listen_to settings
+    settings.on :missing do |e,args|
+      self.on_missing args.first
+    end
+  end
+
+  protected
+
+  def on_missing(name); end
+end
+
+class IgnoreMissing
+  include Missing
+end
+
+class FailOnMissing
+  include Missing
+
+  protected
+
+  def on_missing(name)
+    fail "Setting <#{name}> not found"
+  end
+end
