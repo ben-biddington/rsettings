5e9d815a5ab580616f89ab1d7e2219786bcd1b93
Ben Biddington <ben.biddington@gmail.com>
2013-12-12 19:22:28 +1300

    [WIP] Refactor, educating SettingName

--- lib/rsettings/rsettings.rb
+++ lib/rsettings/rsettings.rb
@@ -3,23 +3,33 @@ class RSettings
     @settings,@names = opts[:settings],opts[:names] 
   end
 
+# todo: eliminate the need for names lookup -- the thing must know itself what name to use
+
   def find(m)
     query = m.to_s.end_with? "?"
 
     m = m.to_s.delete "?" if query
 
-    setting_name = @names.for(m)
+    setting_name = SettingName.new(@names, m)
     
     if query
-      setting_name = @names.for(m)
-
-      value = Setting.new @settings.get(setting_name)
+      value = Setting.new @settings.get(setting_name.to_s)
       
       fail "Unable to convert setting <#{setting_name}> to flag" unless value.truthy?
       
       return value.to_s.downcase === "yes"
     end
 
-    @settings.get(setting_name)    
+    @settings.get(setting_name.to_s)    
+  end
+end
+
+class SettingName
+  def initialize(names, name)
+    @name,@names = name,names
+  end
+  
+  def to_s
+    @names.for @name
   end
 end
