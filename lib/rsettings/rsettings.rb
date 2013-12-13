d99492de0e7801f62ed8956ba6338814d7a26e29
Ben Biddington <ben.biddington@gmail.com>
2013-12-13 19:25:58 +1300

    Classes => file

--- lib/rsettings/rsettings.rb
+++ lib/rsettings/rsettings.rb
@@ -21,26 +21,3 @@ class RSettings
     @settings.get(name).to_s    
   end
 end
-
-class SettingName
-  attr_reader :value
-
-  def initialize(name)
-    @value = name
-  end
-  
-  def eql?(other)
-    return false if other.nil?
-    other.value eqls self.value
-  end
-end
-
-class ConvertibleSettingName
-  def initialize(names, name)
-    @name,@names = name,names
-  end
-  
-  def value
-    @names.for @name
-  end
-end
